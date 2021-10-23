e95c029cf2564ade3357bf9c5515bd54f88185bc
Rohan.sampath <Rohan.sampath@GMRDL11>
2021-10-23 14:30:58 -0700

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+ 
+
+
+
+    class Program
+    {
+
+
+
+
+
+        static void sort(ref List<int> list)
+        {
+            int listLength = list.Count - 1;
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                sorted = true; 
+                for (int i = 0; i < listLength; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        int temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        sorted = false;
+                    }
+                } 
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random rand = new Random();
+
+            int randomNum = rand.Next(0, 10);
+
+            for (int i = 0; i < randomNum; i++)
+            {
+                list.Add(rand.Next(0, 10));
+                Console.Write(list[i].ToString()); 
+            }
+            Console.WriteLine(); 
+            
+
+            sort(ref list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i].ToString());
+            }
+
+
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SelectionSort
+{
+    class Program
+    {
+
+
+
+        static void SelectionSort(ref List<int> list)
+        {
+            int smallestNumber = list[list.Count - 1];
+            int index = 0;
+            int sortedIndex = 0; 
+
+            while (sortedIndex < list.Count)
+            {   
+                smallestNumber = list[sortedIndex];
+                for (int i = sortedIndex; i < list.Count; i++)
+                {
+                    if (list[i] < smallestNumber)
+                    {
+                        smallestNumber = list[i];
+                        index = i; 
+                    }
+                }
+                int temp = list[sortedIndex];
+                list[sortedIndex] = smallestNumber;
+                list[index] = temp;
+                 
+                sortedIndex++;
+                
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+            Random rand = new Random();
+
+            int randomNum = rand.Next(5, 10);
+
+            for (int i = 0; i < randomNum; i++)
+            {
+                list.Add(rand.Next(0, 10));
+                Console.Write(list[i].ToString());
+            }
+            Console.WriteLine();
+
+            SelectionSort(ref list);
+
+            for (int i = 0; i < list.Count; i++)
+            { 
+                Console.Write(list[i].ToString());
+            }
+
+
+        }
+    }
+}
